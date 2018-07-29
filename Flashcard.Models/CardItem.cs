2fd6522b0eff70399b1460738998bda7951f5dd3
ORIFJON NARKULOV <orifjon9@gmail.com>
2018-07-28 23:45:25 -0500

    add Card item model

---
+++ Flashcard.Models/CardItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Flashcard.Models
+{
+    public class CardItem
+    {
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+	}
+}
