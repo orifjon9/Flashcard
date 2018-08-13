62c27b3061c8b7da1d6bc9fe27b949798c8e60b2
ORIFJON NARKULOV <orifjon9@gmail.com>
2018-08-12 22:54:00 -0500

    add Service layer

---
+++ Flashcard.Services/CardService.cs
@@ -0,0 +1,50 @@
+﻿using Flashcard.Models;
+using Flashcard.Repositories;
+using Flashcard.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flashcard.Services
+{
+	public class CardService : ICardService
+	{
+		private readonly ICardRepository _cardRepository;
+		public CardService(ICardRepository cardRepository)
+		{
+			_cardRepository = cardRepository;
+		}
+
+		public async Task<IEnumerable<Card>> ListCards()
+		{
+			return await Task.Run(() => _cardRepository.GetAll());
+		}
+
+		public async Task<Card> GetCardAsync(int id)
+		{
+			return await _cardRepository.GetAsync(id);
+		}
+
+		public async Task<bool> UpdateCardItem(int id, Card phoneToCard)
+		{
+			_cardRepository.Update(phoneToCard);
+			await _cardRepository.CommitAsync();
+			return true;
+		}
+
+		public async Task<bool> CreateCardItemAsync(Card phoneToCard)
+		{
+			await _cardRepository.AddAsync(phoneToCard);
+			await _cardRepository.CommitAsync();
+			return true;
+		}
+
+		public async Task<bool> DeleteCardItem(Card phoneToCard)
+		{
+			_cardRepository.Delete(phoneToCard);
+			await _cardRepository.CommitAsync();
+			return true;
+		}
+	}
+}
---
+++ Flashcard.Services/Interfaces/ICardService.cs
@@ -0,0 +1,18 @@
+﻿using Flashcard.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flashcard.Services.Interfaces
+{
+	public interface ICardService
+	{
+		Task<IEnumerable<Card>> ListCards();
+		Task<Card> GetCardAsync(int id);
+
+		Task<bool> CreateCardItemAsync(Card phoneToCard);
+		Task<bool> UpdateCardItem(int id, Card phoneToCard);
+		Task<bool> DeleteCardItem(Card phoneToCard);
+	}
+}
