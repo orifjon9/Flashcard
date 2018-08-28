4f09ed599be1dd7e507f8d06d19a6c4a397d3bfd
ORIFJON NARKULOV <orifjon9@gmail.com>
2018-08-27 19:50:02 -0500

    add ViewModel layer

---
+++ Flashcard.ViewModels/CardViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Flashcard.ViewModels
+{
+    public class CardViewModel
+    {
+		public int Id { get; set; }
+		[Required]
+		public string Name { get; set; }
+		[Required]
+		public string Description { get; set; }
+	}
+}
